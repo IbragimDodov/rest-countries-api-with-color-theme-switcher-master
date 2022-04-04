76f8bcf64cb1450e22d6ec36c12d6a4e57762fa5
IbragimDodov <utisey@gmail.com>
2022-04-04 10:53:46 +0300

    react project with countries api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="container">
+          <div className="header__inner">
+            <h1>Where in the world?</h1>
+            <div className="dark-mode-block">
+              <img className="dark-mode-block__img" src="img/sleep-mode.png" alt="dark-mode icon" />
+              <p className="dark-mode-block__text">Dark Mode</p>
+            </div>
+          </div>
+        </div>
       </header>
+
+      <main className="main">
+        <div className="container">
+          <div className="main__inner">
+            <div className="main__top-block">
+
+              <div className="main__search">
+                <input className="main__search-input" type="search" placeholder="search for a country..." />
+                <img className="main__search-img" src="img/search.png" alt="search icon" />
+              </div>
+
+              <div className="main__filter">
+                <div className="dropdown">
+                  <div className="dropdown__btn-block">
+                    <button className="dropdown__btn">Filter by Region</button>
+                    <img className="dropdown__img" src="img/dropdown.png" alt="dropdown icon" />
+                  </div>
+                  <div className="dropdown__links-wrapper">
+                    <a className="dropdown__link" href="#">Africa</a>
+                    <a className="dropdown__link" href="#">America</a>
+                    <a className="dropdown__link" href="#">Asia</a>
+                    <a className="dropdown__link" href="#">Evrope</a>
+                    <a className="dropdown__link" href="#">Oceania</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
